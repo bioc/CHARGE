06b22814010ac59594e8b6ea76cf9297533065d1
Benjamin Mayne <BenjaminAdelaide@users.noreply.github.com>
2017-10-23 12:17:29 +1030

    Update plotcvExpr.R

--- R/plotcvExpr.R
+++ R/plotcvExpr.R
@@ -5,17 +5,21 @@
 #' @param cvExpr The output from cvExpr function.
 #' @usage plotcvExpr(cvExpr)
 #' @return Returns a barplot showing the CV for each gene identifier over the region of interest.
-#' @import ggplot2
 #' @author Benjamin Mayne
 #' @export
 
 plotcvExpr <- function(cvExpr){
   
-  datPlot <- data.frame(Gene = names(cvExpr[[1]]), CV = cvExpr[[1]])
+  ### The output from cvExpr contains the CV for each gene 
+  ### Which will be plot in a barplot
+  cvPlot <- barplot(height = cvExpr[[1]], ylab = "CV", las = 2)
   
-  p <-ggplot(datPlot, aes(Gene, CV))
-  p + geom_bar(stat = "identity")  +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-      geom_hline(yintercept = cvExpr[[2]][1:4], color = c("red", "blue", "green", "gold"), show.legend = TRUE)
+  ### The quantiles will be highlighted in four different colours
+  abline(h = cvExpr[[2]][1:4], col = c("red", "blue", "green", "gold"), lwd = 4)
+  legend("topright", cex = 1, col = c("red", "blue", "green", "gold"),
+         fill =  c("red", "blue", "green", "gold"),
+         legend = c("0%", "25%", "50%", "75%"))
+}  
   
-}
+  
+  
